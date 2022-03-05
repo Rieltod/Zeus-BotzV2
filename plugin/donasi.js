4f96f2de847f3dc05618c6ebd1b6a2a9a0188489
Rieltod <99353680+Rieltod@users.noreply.github.com>
2022-03-06 03:57:34 +0800

    Update and rename plugins/donasi.js to plugin/donasi.js

--- plugin/donasi.js
+++ plugin/donasi.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ PULSA TRI3 : 6289625556161
-├ OVO & DANA : 6289625556161
+├ PULSA axis : 083813417529
+├ all axis : 083813417529
 └────
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
